eeabea5e91a9628766f56e48a09f730b0284bea0
Andrew Cole <andrew.cole@illallangi.com>
2024-10-21 22:38:17 +1100

    feat(synchronise): added synchronise action

--- src/illallangi/data/fitness/apps.py
+++ src/illallangi/data/fitness/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 from illallangi.django.data.models.models_manager import ModelsManager
+from illallangi.mastodon.adapters import FitnessAdapter as MastodonAdapter
 
 
 class MastodonConfig(AppConfig):
@@ -17,7 +19,22 @@ class MastodonConfig(AppConfig):
             description="Swimming is a fantastic way to improve overall fitness and well-being.",
         )
 
+        def synchronize() -> None:
+            from illallangi.data.fitness.adapters import (
+                FitnessAdapter as DjangoAdapter,
+            )
+
+            src = MastodonAdapter(
+                **settings.MASTODON,
+            )
+            dst = DjangoAdapter()
+
+            src.load()
+            dst.load()
+
+            src.sync_to(dst)
+
         ModelsManager().add_synchronize(
             name=__name__,
-            synchronize=None,
+            synchronize=synchronize,
         )
